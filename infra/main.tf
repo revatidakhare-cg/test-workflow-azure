965708fb44eb3e5637a4980548a9d70819575bd4
Yadav <pradeep.yadav@capgemini.com>
2026-05-20 14:01:54 +0530

    test

--- infra/main.tf
+++ infra/main.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = "= 1.14.8"
+  required_version = "= 1.15.3"
 
   required_providers {
     azurerm = {
@@ -13,62 +13,31 @@ provider "azurerm" {
   features {}
 }
 
-# -----------------------------------------------------------------------------
-# Resource Group
-# -----------------------------------------------------------------------------
-
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
-}
-
-# -----------------------------------------------------------------------------
-# Storage Account (example workload)
-# -----------------------------------------------------------------------------
-
-resource "azurerm_storage_account" "sa" {
-  name                     = var.storage_account_name
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-
-  tags = var.tags
-}
-
-# Example output that the pipeline attempts to read
-output "example_app_url" {
-  description = "Example endpoint for the deployed resource (storage primary web endpoint)."
-  value       = azurerm_storage_account.sa.primary_web_endpoint
-}
-
-# -----------------------------------------------------------------------------
-# Variables
-# -----------------------------------------------------------------------------
-
-variable "resource_group_name" {
-  description = "Name of the Azure Resource Group."
-  type        = string
-  default     = "tf-example-rg"
-}
+# Basic example Azure resource group to demonstrate deployment
 
 variable "location" {
-  description = "Azure region for all resources."
+  description = "Azure region for resources"
   type        = string
   default     = "eastus"
 }
 
-variable "storage_account_name" {
-  description = "Globally unique name for the storage account. Must be 3-24 lowercase letters and numbers."
+variable "resource_group_name" {
+  description = "Name of the resource group to create"
   type        = string
-  default     = "tfexamplestorageacct01"
+  default     = "example-rg"
 }
 
-variable "tags" {
-  description = "A map of tags to apply to all resources."
-  type        = map(string)
-  default = {
-    environment = "dev"
-    managed-by  = "terraform"
-  }
+resource "azurerm_resource_group" "main" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+output "resource_group_name" {
+  description = "The name of the created resource group"
+  value       = azurerm_resource_group.main.name
+}
+
+output "resource_group_location" {
+  description = "The location of the created resource group"
+  value       = azurerm_resource_group.main.location
 }
